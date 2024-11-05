57dc889560acc9458c20a13ec8034b8bfe2ed87c
Azure_B <benefit5758@naver.com>
2024-11-05 10:07:24 +0900

    [Feat] PHP. 변수 및 상수 출력

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+    
+    $a = 1;
+    
+    echo $a+1;
+    echo "<br/>";
+
+    $a = 2;
+    
+    print($a. "<br/>");
+
+    $first = "Hello";
+
+    echo $first. "world";
+
+    define('TITLE', 'PHP Tutorial');
+    echo $TITLE;
+
+
+
+    ?>
+    
+</body>
+</html>
