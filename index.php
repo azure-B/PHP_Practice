5649e540c532fae071667d9ba8e8bbab30a167e1
Azure_B <benefit5758@naver.com>
2024-11-07 15:55:07 +0900

    [Feat] Incloud 사용 및 DOCUMENT_ROOT의 이해

--- index.php
+++ index.php
@@ -12,23 +12,8 @@
     <title>Hello, world!</title>
   </head>
   <?php
-    $hostname="localhost";
 
-    $dbuserid="HelloSQL";
-
-    $dbpasswd="13467979!Q";
-
-    $dbname="HelloSQL";
-
-    
-
-    $mysqli = new mysqli($hostname, $dbuserid, $dbpasswd, $dbname);
-
-    if ($mysqli->connect_errno) {
-
-        die('Connect Error: '.$mysqli->connect_error);
-
-    }
+    include $_SERVER["DOCUMENT_ROOT"]."/dbcon.php";
 
     $result = $mysqli->query("select * from CRUD") or die("query error => ".$mysqli->error);
     
--- view.php
+++ view.php
@@ -1,22 +1,7 @@
 <?php
 ini_set( 'display_errors', '0' );
-$hostname="localhost";
 
-$dbuserid="HelloSQL";
-
-$dbpasswd="13467979!Q";
-
-$dbname="HelloSQL";
-
-
-
-$mysqli = new mysqli($hostname, $dbuserid, $dbpasswd, $dbname);
-
-if ($mysqli->connect_errno) {
-
-    die('Connect Error: '.$mysqli->connect_error);
-
-}
+include $_SERVER["DOCUMENT_ROOT"]."/dbcon.php";
 
 $ID=$_GET["ID"];
 $result = $mysqli->query("select * from CRUD where ID=".$ID) or die("query error => ".$mysqli->error);
